8cd8340558552c8e710086ea177a449184d4064a
Rajat Soni <rajatsonibkn2002@gmail.com>
2024-07-12 01:44:50 +0530

    cron testing

--- cron-service/start-crons.mjs
+++ cron-service/start-crons.mjs
@@ -2,16 +2,16 @@ import * as cron from "node-cron";
 import { fetchContributions as fetchEra1Contributions } from "./crons/era-1-cron.mjs";
 import { fetchContributions as fetchEra2Contributions } from "./crons/era-2-cron.mjs";
 
-cron.schedule("0 0 * * *", () => {
+cron.schedule("*/5 * * * *", () => {
   console.log("Cron Ran for Era-1");
   fetchEra1Contributions("base");
   fetchEra1Contributions("pulsechain");
 });
 
-cron.schedule("0 0 * * *", () => {
-  console.log("Cron Ran for Era-2");
-  fetchEra2Contributions("base");
-  fetchEra2Contributions("pulsechain");
-});
+// cron.schedule("*/5 * * * *", () => {
+//   console.log("Cron Ran for Era-2");
+//   fetchEra2Contributions("base");
+//   fetchEra2Contributions("pulsechain");
+// });
 
 setInterval(() => {}, 1000);
