e5c836617b19ece349b741d0fc57887f70303979
Rajat Soni <rajatsonibkn2002@gmail.com>
2024-07-12 02:11:44 +0530

    test era 2 cron

--- cron-service/start-crons.mjs
+++ cron-service/start-crons.mjs
@@ -7,16 +7,16 @@ import { fetchSecretsList } from "../secrets-manager/secrets-manager.mjs";
 const secrets = await fetchSecretsList();
 await mongoose.connect(secrets?.MONGODB_CONNECTION_STRING);
 
-cron.schedule("*/3 * * * *", () => {
-  console.log("Cron Ran for Era-1");
-  fetchEra1Contributions("base");
-  fetchEra1Contributions("pulsechain");
-});
-
 // cron.schedule("*/5 * * * *", () => {
-//   console.log("Cron Ran for Era-2");
-//   fetchEra2Contributions("base");
-//   fetchEra2Contributions("pulsechain");
+//   console.log("Cron Ran for Era-1");
+//   fetchEra1Contributions("base");
+//   fetchEra1Contributions("pulsechain");
 // });
+
+cron.schedule("*/3 * * * *", () => {
+  console.log("Cron Ran for Era-2");
+  fetchEra2Contributions("base");
+  fetchEra2Contributions("pulsechain");
+});
 
 setInterval(() => {}, 1000);
