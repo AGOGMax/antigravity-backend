c6aecd2cd6b65bd408995785b983721b5cabb3fa
Rajat Soni <rajatsonibkn2002@gmail.com>
2024-09-10 22:00:55 +0530

    removed hardcoded subgraph URL

--- master-service/utils/lottery.mjs
+++ master-service/utils/lottery.mjs
@@ -27,8 +27,7 @@ const fetchAndAttachAddresses = async (lotteryBatch) => {
     `;
 
   const response = await axios.post(
-    "https://api.studio.thegraph.com/query/64293/ag-base-sepolia/version/latest" ||
-      secrets?.ERA_3_SUBGRAPH_URL,
+    secrets?.ERA_3_SUBGRAPH_URL,
     {
       query: usersQuery,
     },
