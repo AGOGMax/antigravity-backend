b4602d0226ee44ae2e8e771237046965d434d354
Rajat Soni <rajatsonibkn2002@gmail.com>
2024-11-25 12:21:26 +0530

    added difference, and fixed total points for the api

--- master-service/utils/contributions.mjs
+++ master-service/utils/contributions.mjs
@@ -30,7 +30,44 @@ const fetchEra2Points = async () => {
       },
     ])
     .exec();
-  return totalPoints;
+
+  const result = await pointsModel
+    .aggregate([
+      {
+        $match: {
+          era: { $in: [1, 2] },
+        },
+      },
+      {
+        $group: {
+          _id: { walletAddress: "$walletAddress", era: "$era" },
+          points: { $sum: "$points" },
+        },
+      },
+      {
+        $group: {
+          _id: "$_id.walletAddress",
+          totalPoints: { $sum: "$points" },
+          era2Points: {
+            $sum: {
+              $cond: [{ $eq: ["$_id.era", 2] }, "$points", 0],
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          address: "$_id",
+          totalPoints: 1,
+          era2Points: 1,
+          difference: { $subtract: ["$totalPoints", "$era2Points"] },
+        },
+      },
+    ])
+    .exec();
+
+  return result;
 };
 
 export { fetchEra1Contributors, fetchEra2Points };
