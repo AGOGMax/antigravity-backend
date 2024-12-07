3cdf7c618e4e7d55942a520e62a6801711760116
Rajat Soni <rajatsonibkn2002@gmail.com>
2024-12-07 19:30:02 +0530

    optimised secrets manager logic

--- secrets-manager/secrets-manager.mjs
+++ secrets-manager/secrets-manager.mjs
@@ -1,49 +1,28 @@
 import {
   SecretsManagerClient,
-  ListSecretsCommand,
-  BatchGetSecretValueCommand,
+  GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
 
 const client = new SecretsManagerClient({
   region: "us-east-1",
 });
 
-const createBatches = (array, batchSize) => {
-  const batches = [];
-  for (let i = 0; i < array.length; i += batchSize) {
-    batches.push(array.slice(i, i + batchSize));
-  }
-  return batches;
-};
-
 const fetchSecretsList = async () => {
   try {
-    const fetchSecretsListCommand = new ListSecretsCommand({ MaxResults: 50 });
-    const secretsList = await client.send(fetchSecretsListCommand);
-    const secretNames = secretsList?.SecretList?.map((secret) => secret?.Name);
-    const batches = createBatches(secretNames, 20);
+    const environment = process.env.ENV || "TEST";
+    const secretName = `${environment}_SECRETS`;
 
-    const batchGetSecretPromises = batches.map(async (batch) => {
-      const input = { SecretIdList: batch };
-      const secretValueCommand = new BatchGetSecretValueCommand(input);
-      return client.send(secretValueCommand);
-    });
+    const command = new GetSecretValueCommand({ SecretId: secretName });
+    const response = await client.send(command);
 
-    const secretsValuesResponses = await Promise.all(batchGetSecretPromises);
-    const allSecrets = secretsValuesResponses.flatMap(
-      (response) => response?.SecretValues
-    );
-
-    let secretsMapping = {};
-
-    allSecrets?.forEach((secretValue) => {
-      secretsMapping = {
-        ...secretsMapping,
-        ...JSON.parse(secretValue?.SecretString),
-      };
-    });
-    return secretsMapping;
+    if (response?.SecretString) {
+      const secretsMapping = JSON.parse(response.SecretString);
+      return secretsMapping;
+    } else {
+      throw new Error("SecretString not found in the response");
+    }
   } catch (error) {
+    console.error("Error fetching SECRETS:", error);
     throw error;
   }
 };
